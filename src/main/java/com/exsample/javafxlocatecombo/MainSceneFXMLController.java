cd0c4e54a1dbde37859d916f8f8cf6fd305ac984
unknown <>
2016-04-30 21:38:15 +0900

    enumにフィールドとメソッドを用意し、ComboBoxのレンダリングで文字を表示。

--- src/main/java/com/exsample/javafxlocatecombo/MainSceneFXMLController.java
+++ src/main/java/com/exsample/javafxlocatecombo/MainSceneFXMLController.java
@@ -10,6 +10,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
 
 public class MainSceneFXMLController implements Initializable {
 
@@ -20,11 +23,26 @@ public class MainSceneFXMLController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        ObservableList<ParakeetEnum> list = FXCollections.observableArrayList(ParakeetEnum.values());
+        Callback<ListView<ParakeetEnum>, ListCell<ParakeetEnum>> cellFactory
+                = (ListView<ParakeetEnum> param) -> new ListCell<ParakeetEnum>() {
+            @Override
+            protected void updateItem(ParakeetEnum item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item != null && !empty) {
+                    setText(item.getName());
+                }
+            }
+        };
+        ObservableList<ParakeetEnum> list
+                = FXCollections.observableArrayList(ParakeetEnum.values());
         comboParakeet.getItems().addAll(list);
-        comboParakeet.valueProperty().addListener((ObservableValue<? extends ParakeetEnum> observable, ParakeetEnum oldValue, ParakeetEnum newValue) -> {
+        comboParakeet.setButtonCell(cellFactory.call(null));
+        comboParakeet.setCellFactory(cellFactory);
+        comboParakeet.valueProperty().addListener((
+                ObservableValue<? extends ParakeetEnum> observable,
+                ParakeetEnum oldValue, ParakeetEnum newValue) -> {
             if (Objects.nonNull(newValue)) {
-                label.setText(newValue.name());
+                label.setText(newValue.getName());
             }
         });
     }
--- src/main/java/com/exsample/javafxlocatecombo/ParakeetEnum.java
+++ src/main/java/com/exsample/javafxlocatecombo/ParakeetEnum.java
@@ -1,7 +1,17 @@
 package com.exsample.javafxlocatecombo;
 
 public enum ParakeetEnum {
-    PEACH_FACED_LOVEBIRD,
-    COCKATIEL,
-    BUDGERIGAR;
+    PEACH_FACED_LOVEBIRD("小桜インコ"),
+    COCKATIEL("オカメインコ"),
+    BUDGERIGAR("セキセイインコ");
+
+    final private String name;
+
+    private ParakeetEnum(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
